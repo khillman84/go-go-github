ecc2c5fd4db79cf12f7ef929473204a8583d2e7e
Kyle Hillman <khillman84@gmail.com>
2017-04-04 14:38:29 -0700

    got project working again

--- GoGoGitHub/GoGoGitHub/AppDelegate.swift
+++ GoGoGitHub/GoGoGitHub/AppDelegate.swift
@@ -20,11 +20,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        if let token = UserDefaults.standard.getAccessToken() {
-            print(token)
-        } else {
-            presentAuthController()
-        }
+        presentAuthController()
+//        if let token = UserDefaults.standard.getAccessToken() {
+//            print(token)
+//        } else {
+//        }
         
         return true
     }
